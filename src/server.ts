43d6a06e8997206d24554f145759af9a899c3bf4
Yuguo Zhao <cheerzhao77@gmail.com>
2021-07-20 14:28:29 -0500

    Feat: add database.ts and database.json, try to connect node to postgres

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ import bodyParser from "body-parser";
 const app: express.Application = express();
 const address: string = "0.0.0.0:3000";
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get("/", function (req: Request, res: Response) {
   res.send("Hello World!");
